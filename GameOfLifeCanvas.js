5cede11e80765bf90d1db39c077027afbbe95b3e
stephan.koeninger <mail@stekoe.de>
2014-09-23 20:31:32 +0200

    Added option to draw circles instead of squares

--- GameOfLifeCanvas.js
+++ GameOfLifeCanvas.js
@@ -11,7 +11,10 @@ function GameOfLiveCanvas(canvasId, cols, rows, options) {
 	}
 	var canvasId = canvasId;
 	var defaultOptions = {
-		'size' : 5
+		'size' : 5,
+		'colorDead' : '#dddddd',
+		'colorAlive' : '#000000',
+		'figure' : 'rect'
 	}
 
 	var options = mergeOptions(defaultOptions, options);
@@ -39,16 +42,21 @@ function GameOfLiveCanvas(canvasId, cols, rows, options) {
 		}
 
 		ctx.beginPath();
-		ctx.rect(dx, dy, options.size, options.size);
+		
+		if(options.figure == "arc") {
+			ctx.arc(dx + options.size/2, dy + options.size/2, options.size/2, 0, 2 * Math.PI);
+		} else {
+			ctx.rect(dx, dy, options.size, options.size);
+		}
 		ctx.fillStyle = color;
 		ctx.fill();
 	}
 
 	this.alive = function(x, y) {
-		drawPoint(x, y, 'black')
+		drawPoint(x, y, options.colorAlive)
 	}
 	this.dead = function(x, y) {
-		drawPoint(x, y, '#efefef')
+		drawPoint(x, y, options.colorDead)
 	}
 
 	this.clear = function() {
